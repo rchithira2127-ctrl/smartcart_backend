6523ac55fe19a313e1b479738cce60fc3ab7441f
rchithira2127-ctrl <rchithira2127@gmail.com>
2026-03-06 18:59:11 +0530

    Added product model and admin configuration

--- store/models.py
+++ store/models.py
@@ -3,3 +3,14 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
